fe4a24652f8cdb07ee5f94735c58548c9ca50fcf
kevinsvni <kvnsavani@gmail.com>
2023-10-13 07:07:36 -0400

    ReactRedux 5 - Actions

--- REDUX-DEMO/index.js
+++ REDUX-DEMO/index.js
@@ -1,1 +1,13 @@
-console.log('From Index')
+const BUY_CAKE = 'BUY_CAKE'
+
+{
+    type: BUY_CAKE
+    info: 'First redux action'
+}
+
+function buyCake() {
+    return{
+        type: BUY_CAKE,
+        info: 'First redux action'
+    }
+}
